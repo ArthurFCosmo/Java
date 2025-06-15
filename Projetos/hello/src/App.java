13d05c57433187de2b71ba414209cdbbea9e122a
Arthur Filipe Cosmo <arthurcosmoservices@gmail.com>
2025-06-15 12:01:10 -0300

    Colocando primeiro projeto no repositório

---
+++ Projetos/hello/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Por aqui amamos " + args[0]);
+        System.out.println("Tudo isso graças ao " + args[1]);
+    }
+}
